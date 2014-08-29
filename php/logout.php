e9986758489f9ae366c2f41fda99376298e3d856
xxvii27 <llesmana@ucsd.edu>
2014-08-29 15:26:35 -0700

    update status when logged in and logged out

--- php/logout.php
+++ php/logout.php
@@ -1,6 +1,8 @@
 <?php
 
 session_start();
+$name = $_SESSION['name'];
+mysql_query("UPDATE user SET status=0 WHERE email = '$name' ");
 session_destroy();
 
 header("Location: http://104.131.199.129:83/logout.html");
