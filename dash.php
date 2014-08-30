de76d20885bb7ca8c0f2a73ca9c8b1edcca96609
xxvii27 <llesmana@ucsd.edu>
2014-08-29 20:09:20 -0700

    adding add user modal

--- dash.php
+++ dash.php
@@ -84,6 +84,14 @@ $name = $_SESSION['name'];
       </div>
   </div>
 
+  <div class="modal fade bs-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal-sm">
+          <div class="modal-content">
+              ...
+          </div>
+      </div>
+  </div>
+
 </body>
 
 </html>
