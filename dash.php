ac702afd2363f437c41e7090b8d4c51566ae093b
xxvii27 <llesmana@ucsd.edu>
2014-08-29 22:00:18 -0700

    add user by user

--- dash.php
+++ dash.php
@@ -84,7 +84,7 @@ $name = $_SESSION['name'];
       </div>
   </div>
 
-  <div class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+  <div class="modal fade bs-example-modal-lg" id="addUserDialog" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
       <div class="modal-dialog modal-lg">
           <div class="modal-content">
               <div class="modal-header">
