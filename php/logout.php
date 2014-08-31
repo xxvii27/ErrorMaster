3367827705706c58e3fe380011cf4386fde3ed98
xxvii27 <llesmana@ucsd.edu>
2014-08-31 10:54:04 -0700

    required

--- php/logout.php
+++ php/logout.php
@@ -17,8 +17,13 @@ session_start();
 
 
  $name = $_SESSION['name'];
+ $access =  $_SESSION['access'];
  echo $name;
- mysql_query("UPDATE user SET status=0 WHERE email='$name' ");
+ if($access=="owner")
+ 	mysql_query("UPDATE user SET status=0 WHERE email='$name' ");
+ else
+ 	mysql_query("UPDATE members SET status=0 WHERE email='$name' ");
+ 
  session_destroy();
 
  header("Location: http://104.131.199.129:83/logout.html");
