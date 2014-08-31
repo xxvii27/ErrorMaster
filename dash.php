38727a159c3b22f3c3c6d0909c8c2673473c1d8e
xxvii27 <llesmana@ucsd.edu>
2014-08-30 23:19:30 -0700

    try delete user

--- dash.php
+++ dash.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
 date_default_timezone_set('America/Los_Angeles');
 $date = date('m/d/Y h:i:s a', time());
 $name = $_SESSION['name'];
