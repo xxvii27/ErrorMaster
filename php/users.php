91d7a4ce8a1ca325fc12fb4921d8dfe03f4bced8
xxvii27 <llesmana@ucsd.edu>
2014-08-30 21:56:05 -0700

    try delete user

--- php/users.php
+++ php/users.php
@@ -28,8 +28,8 @@ function printUser($username, $status){
 	    echo "<span class='statoff'>Offline";
 
 	echo"</span>" ;
-	echo "<button type='button' class='btn btn-default pull-right delete'><span class='glyphicon glyphicon-minus'></span></a>";
-	echo "<button type='button' class='btn btn-default pull-right edit'><span class='glyphicon glyphicon-cog'></span></a></td>";
+	echo "<button type='button' class='btn btn-default pull-right delete'><span class='glyphicon glyphicon-minus'></span></button></a>";
+	echo "<button type='button' class='btn btn-default pull-right edit'><span class='glyphicon glyphicon-cog'></span></button></a></td>";
 	echo"</tr>";
 }
 
