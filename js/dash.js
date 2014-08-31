4aa2b9c7535fc12bc6da5caa05b9958bc7cb591f
xxvii27 <llesmana@ucsd.edu>
2014-08-30 22:00:30 -0700

    try delete user

--- js/dash.js
+++ js/dash.js
@@ -165,7 +165,7 @@ window.onload = function (){
 
     $(document).on("click", '.delete', function (e) {
 
-       alert ( $(this).parentNode.previousSibling.previousSibling.previousSibling.innerHTML );
+       alert ( $(this).parent() );
 
     });
 
