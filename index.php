d9b0cc5048fc02beb48ff33381539bea5829e94c
xxvii27 <llesmana@ucsd.edu>
2014-09-01 14:24:56 -0700

    forgot password

--- index.php
+++ index.php
@@ -75,7 +75,8 @@
 	        <input type="text" class="form-control" placeholder="Last Name"  name='last' required autofocus>
 	        <input type="email" class="form-control" placeholder="Email address" name='email' required autofocus>
 	        <input type="password" class="form-control" placeholder="Password" name='password' required>
-	        <button class="btn btn-primary btn-large sign-up-button" name='submit' type="submit">Sign Me Up!</button>
+            <input type="password" class="form-control" placeholder="Team Code" name='code' required>
+            <button class="btn btn-primary btn-large sign-up-button" name='submit' type="submit">Sign Me Up!</button>
 	      </form>
     	</div> <!-- /container -->
 
--- php/signup.php
+++ php/signup.php
@@ -2,13 +2,13 @@
 
 //Database Connection 
 function connectDB (){
-	define('DB_HOST', 'localhost');
-	define('DB_NAME', 'userinfo');
-	define('DB_USER','root');
-	define('DB_PASSWORD','ohanajumba');
+    define('DB_HOST', 'localhost');
+    define('DB_NAME', 'userinfo');
+    define('DB_USER','root');
+    define('DB_PASSWORD','ohanajumba');
 
-	$con=mysql_connect(DB_HOST,DB_USER,DB_PASSWORD) or die("Failed to connect to MySQL: " . mysql_error()); 
-	$db=mysql_select_db(DB_NAME,$con) or die("Failed to connect to MySQL: " . mysql_error());
+    $con=mysql_connect(DB_HOST,DB_USER,DB_PASSWORD) or die("Failed to connect to MySQL: " . mysql_error());
+    $db=mysql_select_db(DB_NAME,$con) or die("Failed to connect to MySQL: " . mysql_error());
 
 }
 
@@ -17,48 +17,49 @@ function connectDB (){
 //Getting information from post
 function  insertUser(){
 
-	$firstname = $_POST["first"];
-	$lastname =  $_POST["last"];
-	$email = $_POST["email"];
-	$password = $_POST["password"];
+    $firstname = $_POST["first"];
+    $lastname =  $_POST["last"];
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+    $code = $_POST['code'];
 
-	//Encryption for password
-	$password = hash("sha512", $password);
+    //Encryption for password
+    $password = hash("sha512", $password);
 
-	//MySQL query command 
-	$command = "INSERT INTO user (id, firstname, lastname, email, password, status) 
-				VALUES (NULL, '$firstname','$lastname','$email','$password', 0)";
+    //MySQL query command
+    $command = "INSERT INTO user (id, firstname, lastname, email, password, code, status)
+				VALUES (NULL, '$firstname','$lastname','$email','$password',$code ,0)";
 
 
-           if( $query = mysql_query($command) or die( mysql_error() ) ){
-               header("Location: http://104.131.199.129:83/signed.html");
-           }
+    if( $query = mysql_query($command) or die( mysql_error() ) ){
+        header("Location: http://104.131.199.129:83/signed.html");
+    }
 }
 
 
 //signup
 function sign(){
-   
+
     //check duplicates
     $command = mysql_query("SELECT * FROM user WHERE email = '$_POST[email]'") or die( mysql_error() );
-    if( !$row = mysql_fetch_array($command) or die( mysql_error() ) ) { 
-         insertUser(); 
-    } 
-    else { 
+    if( !$row = mysql_fetch_array($command) or die( mysql_error() ) ) {
+        insertUser();
+    }
+    else {
 
-         header("Location: http://104.131.199.129:83/index.php");
+        header("Location: http://104.131.199.129:83/index.php");
 
     }
 
-   
+
 }
 
 if(isset($_POST['submit']))
 {
-	
-	connectDB();
-	//sign user
-	sign();
+
+    connectDB();
+    //sign user
+    sign();
 }
 
 
