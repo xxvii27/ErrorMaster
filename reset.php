1f03f1fe319a89645e262136620de5085d016732
xxvii27 <llesmana@ucsd.edu>
2014-09-01 12:16:22 -0700

    forgot password

---
+++ reset.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xxvii27
+ * Date: 9/1/14
+ * Time: 11:50 AM
+ */
+
+session_start();
+
+$email = $_SESSION['email'];
+if($email === null){
+    http_response_code(403);
+    header('Location: http://104.131.199.129:83/error/forbidden403.html');
+    exit();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="Your Mother">
+    <link rel="icon" href="../../favicon.ico"> <!-- ADD A FAVICON HEERE -->
+
+    <title>Sign In</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="./css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="./css/signin.css" rel="stylesheet">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
+
+</head>
+
+<body>
+<div class="container">
+    <form class="form-signin" action="./php/edit.php" method="POST" accept-charset="UTF-8">
+        <h2>Please enter new password</h2>
+        <input type="password" class="form-control" placeholder="Password" id="password" name="password" maxlength="30" required>
+        <div id='logbuttons'>
+            <button class="btn btn-lg btn-primary" type="submit">Reset</button>
+        </div>
+    </form>
+</div> <!-- /container -->
+
+
--- resetted.php
+++ resetted.php
@@ -3,18 +3,43 @@
  * Created by PhpStorm.
  * User: xxvii27
  * Date: 9/1/14
- * Time: 11:13 AM
+ * Time: 12:04 PM
  */
 
 session_start();
 
-$email = $_POST['email'];
+//Database Connection
+function connectDB (){
+    define('DB_HOST', 'localhost');
+    define('DB_NAME', 'userinfo');
+    define('DB_USER','root');
+    define('DB_PASSWORD','ohanajumba');
 
-$_SESSION['email'] = $email;
+    $con=mysql_connect(DB_HOST,DB_USER,DB_PASSWORD) or die("Failed to connect to MySQL: " . mysql_error());
+    $db=mysql_select_db(DB_NAME,$con) or die("Failed to connect to MySQL: " . mysql_error());
 
-mail($email, "Reset Password", "To reset password please go to the following link: ", "ErrorMaster Admin");
+}
+
+connectDB();
+
+$email = $_SESSION['email'];
+if($email === null){
+    http_response_code(403);
+    header('Location: http://104.131.199.129:83/error/forbidden403.html');
+    exit();
+}
+
+$password = $_POST['password'];
+
+$password = hash("sha512", $password);
+
+$command = "UPDATE FROM user SET password='$password' WHERE email='$email'";
+
+mysql_query($command);
+
 
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -24,7 +49,7 @@ mail($email, "Reset Password", "To reset password please go to the following lin
     <meta name="author" content="Your Mother">
     <link rel="icon" href="../../favicon.ico"> <!-- ADD A FAVICON HEERE -->
 
-    <title>Reset</title>
+    <title>Resetted</title>
 
     <!-- Bootstrap core CSS -->
     <link href="./css/bootstrap.min.css" rel="stylesheet">
@@ -35,7 +60,7 @@ mail($email, "Reset Password", "To reset password please go to the following lin
 
 <body>
 <div class="container">
-    <h1 class="page-header">Password Reset link have been sent to your Email !!!</h1>
+    <h1 class="page-header">Pasword resetted  !!!!</h1>
     <br/>
     <a class="btn btn-lg btn-primary" href="http://104.131.199.129:83/index.php">Login</a>
 </div> <!-- /container -->
