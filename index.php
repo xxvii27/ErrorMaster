f82f5dae70b7cc949ffa4625fa0c8addc9ec545e
xxvii27 <llesmana@ucsd.edu>
2014-09-02 15:56:35 -0700

    collecting errors

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>Welcome Login</title>
+    <title>ErrorMaster</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
