a287b5a53f4d54b8712fe8d4874e7143312d707a
xxvii27 <llesmana@ucsd.edu>
2014-09-02 14:20:28 -0700

    collecting errors

--- index.php
+++ index.php
@@ -27,6 +27,7 @@
     <link href="/css/bootstrap-responsive.css" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <script src="/js/bootstrap.min.js"></script>
+    <script src="js/log_error.js"></script>
 
     <!-- Fav and touch icons (can edit these later) -->
     <link rel="shortcut icon" href="../assets/ico/favicon.png">
