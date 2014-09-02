b0ea4743c96979d933d17188aeca43406db4bd0d
xxvii27 <llesmana@ucsd.edu>
2014-09-02 14:09:16 -0700

    collecting errors

---
+++ js/log_error.js
@@ -0,0 +1,67 @@
+/**
+ * Created by xxvii27 on 9/2/14.
+ */
+window.onerror = function(msg, url, line)
+{
+
+    function createXHR()
+    {
+        try { return new XMLHttpRequest(); } catch(e) {}
+        try { return new ActiveXObject("Msxml2.XMLHTTP.6.0"); } catch (e) {}
+        try { return new ActiveXObject("Msxml2.XMLHTTP.3.0"); } catch (e) {}
+        try { return new ActiveXObject("Msxml2.XMLHTTP"); } catch (e) {}
+        try { return new ActiveXObject("Microsoft.XMLHTTP"); } catch (e) {}
+
+        return null;
+    }
+
+    function sendRequest(url, payload)
+    {
+        var xhr = createXHR();
+        if (xhr)
+        {
+            xhr.open("POST",url,true);
+            xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+            xhr.onreadystatechange = function(){
+                if (xhr.readyState == 4  && xhr.status == 200)
+                    alert("error logged");
+            };
+            xhr.send(payload);
+        }
+
+    }
+
+    function encodeValue(val)
+    {
+        var encodedVal;
+        if (!encodeURIComponent)
+        {
+            encodedVal = escape(val);
+            /* fix the omissions */
+            encodedVal = encodedVal.replace(/@/g, '%40');
+            encodedVal = encodedVal.replace(/\//g, '%2F');
+            encodedVal = encodedVal.replace(/\+/g, '%2B');
+        }
+        else
+        {
+            encodedVal = encodeURIComponent(val);
+            /* fix the omissions */
+            encodedVal = encodedVal.replace(/~/g, '%7E');
+            encodedVal = encodedVal.replace(/!/g, '%21');
+            encodedVal = encodedVal.replace(/\(/g, '%28');
+            encodedVal = encodedVal.replace(/\)/g, '%29');
+            encodedVal = encodedVal.replace(/'/g, '%27');
+        }
+        /* clean up the spaces and return */
+        return encodedVal.replace(/\%20/g,'+');
+    }
+
+    var master = "llesmana@ucsd.edu"
+
+    var payload = "msg=" + encodeValue(msg) + '&url=' + encodeValue(url) + "&line=" + encodeValue(line) + "&master=" + encodeValue(master);
+    var url = "http://104.131.199.129:83/php/log_error.php";
+    sendRequest(url, payload);
+
+
+
+}
---
+++ php/log_error.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xxvii27
+ * Date: 9/2/14
+ * Time: 12:30 PM
+ */
+
+
+/* Helper functions */
+function gpc($name)
+{
+    if (isset($_GET[$name]))
+        return $_GET[$name];
+    else if (isset($_POST[$name]))
+        return $_POST[$name];
+    else if (isset($_COOKIE[$name]))
+        return $_COOKIE[$name];
+    else
+        return "";
+}
+
+//Database Connection
+function connectDB (){
+    define('DB_HOST', 'localhost');
+    define('DB_NAME', 'userinfo');
+    define('DB_USER','root');
+    define('DB_PASSWORD','ohanajumba');
+
+    $con=mysql_connect(DB_HOST,DB_USER,DB_PASSWORD) or die("Failed to connect to MySQL: " . mysql_error() );
+    $db=mysql_select_db(DB_NAME,$con) or die("Failed to connect to MySQL: " . mysql_error() );
+
+}
+
+
+function logError($occured, $name, $line, $master){
+
+        $command="INSERT INTO errors (occurred, name, log, master) VALUES ('$occured', '$name', '$line', '$master')";
+        mysql_query($command);
+}
+
+
+connectDB();
+
+$message = htmlentities(substr(urldecode(gpc("msg")),0,1024));
+$url = htmlentities(substr(urldecode(gpc("url")),0,1024));
+$line = htmlentities(substr(urldecode(gpc("line")),0,1024));
+$master = htmlentities(substr(urldecode(gpc("master")),0,1024));
+
+$date = date('m/d/Y h:i:s a', time());
+
+logError($date, $message, $line, $master);
