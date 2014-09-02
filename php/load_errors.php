12ca1f54c758cd98bfec9c676a6e3f3e8406d509
xxvii27 <llesmana@ucsd.edu>
2014-09-02 15:39:36 -0700

    collecting errors

--- php/load_errors.php
+++ php/load_errors.php
@@ -6,10 +6,55 @@
  * Time: 3:07 PM
  */
 
-function printErrors(){
+session_start();
+
+$access = $_SESSION['status'];
+
+//Database Connection
+function connectDB (){
+    define('DB_HOST', 'localhost');
+    define('DB_NAME', 'userinfo');
+    define('DB_USER','root');
+    define('DB_PASSWORD','ohanajumba');
+
+    $con=mysql_connect(DB_HOST,DB_USER,DB_PASSWORD) or die("Failed to connect to MySQL: " . mysql_error() );
+    $db=mysql_select_db(DB_NAME,$con) or die("Failed to connect to MySQL: " . mysql_error() );
 
 }
 
-function reloadErrors(){
 
+function printErrors($time, $count, $name, $severity){
+
+    echo "<tr>";
+    print "<td>$time </td>";
+    echo "<td>0</td>";
+    echo "<td>$name</td>";
+    echo "<td>0</td>";
+    echo"</tr>";
+
+}
+
+function reloadErrors($username){
+    //Commence Query
+    $queryErrors = "SELECT * FROM errors WHERE master = '$username'";
+
+    $result = mysql_query($queryErrors);
+
+    while ($row = mysql_fetch_array($result)) {
+        printUser($row['occured'], $row['count'], $row['name'], $row['severity']);
+    }
+
+}
+
+connectDB();
+
+$username = htmlentities(substr(urldecode(gpc("user")), 0, 1024));
+
+if($access == 'owner')
+    reloadErrors($username);
+else{
+    $result = mysql_query("SELECT * FROM members WHERE email='$username'");
+    $row = mysql_fetch_array($result);
+    $master= $row['master'];
+    reloadErrors($master);
 }
