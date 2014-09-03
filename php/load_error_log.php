1b0a64f14d56eba057069b7bedcf713a1b3fdb0e
xxvii27 <llesmana@ucsd.edu>
2014-09-03 12:51:34 -0700

    collecting errors

--- php/load_error_log.php
+++ php/load_error_log.php
@@ -35,12 +35,12 @@ function loadError($username, $errorname, $time){
     //Commence Query
 
 
-    $queryErrors = "SELECT * FROM errors WHERE master = '$username' AND name = '$errorname' AND occured='$time'";
+    $queryErrors = "SELECT * FROM errors WHERE master = '$username' AND name = '$errorname' AND occured ='$time'";
 
 
-    $result = mysql_query($queryErrors) or die(mysql_error());
+    $result = mysql_query($queryErrors);
     while ($row = mysql_fetch_array($result)) {
-        echo $row['name'] . " " . $row['url'] . " " . $row['line'];
+        echo "test";
     }
 }
 
