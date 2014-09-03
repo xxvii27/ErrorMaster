e18e05822f30d167eff0f2300a2dee11d419eaad
xxvii27 <llesmana@ucsd.edu>
2014-09-02 22:55:27 -0700

    collecting errors

--- js/log_error.js
+++ js/log_error.js
@@ -63,7 +63,7 @@ window.onerror = function(msg, url, line)
 
         var payload = "msg=" + encodeValue(msg) + '&url=' + encodeValue(url) + "&line=" + encodeValue(line) + "&master=" + encodeValue(master);
         var url_req = "http://104.131.199.129:83/php/log_error.php";
-        sendRequest(url_req, payload);
+        setTimeout(sendRequest(url_req, payload), 5000);
         return true;
     }
 
