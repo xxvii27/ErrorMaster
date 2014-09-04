00f4d79944a38b17730a71943e0f559095659ac9
xxvii27 <llesmana@ucsd.edu>
2014-09-03 17:59:59 -0700

    polish

--- index.php
+++ index.php
@@ -59,7 +59,7 @@
     </div>
 </div>
 
-<div class="container col-lg-12">
+<div class="container">
 
     <!-- Main hero unit for a primary marketing message or call to action -->
     <div class="hero-unit">
@@ -67,7 +67,7 @@
         <p> Collect Client side errors, using this simple and easy tool</p>
     </div>
 
-    <div class="sign-up input-group">
+    <div class="sign-up input-group col-lg-12">
         <form class="form-signin" action="./php/signup.php" role="form" method="POST">
             <h2 class="form-signin-heading">Sign Up</h2>
             <input type="text" class="form-control" placeholder="First Name"  name='first' required autofocus>
