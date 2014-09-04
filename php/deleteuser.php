eb1d0c0a27d0d6f1556ba8d7fc65b25aea283a59
xxvii27 <llesmana@ucsd.edu>
2014-09-04 02:35:29 -0700

    bugs

--- php/deleteuser.php
+++ php/deleteuser.php
@@ -167,7 +167,8 @@ if($master === "admin@errormaster.com"){
 else
     $command = "DELETE FROM members WHERE email = '$username' AND master='$master'";
 
-mysql_query($command);
+$result = mysql_query($command);
+echo mysql_num_rows($result);
 
 if($master === "admin@errormaster.com")
     reloadUsersAdmin($master);
