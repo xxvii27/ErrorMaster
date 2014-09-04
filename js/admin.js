4059d4e8eb919599b0aa8645c1a82b15b25943de
xxvii27 <llesmana@ucsd.edu>
2014-09-04 03:09:55 -0700

    bugs

--- js/admin.js
+++ js/admin.js
@@ -142,7 +142,6 @@ window.onload = function (){
                     var last = document.getElementsByName("last")[1].value;
                     var password = document.getElementsByName("password")[1].value;
                     var user = document.getElementById('userid').innerText;
-                    alert(user);
 
                     if(first === "" || last === "" || email === "" || password === "")
                         alert("Empty form detected, make sure you enter all information");
