581658d0ac99c2b8d6e3fde0d2d62bd6f898a4a4
xxvii27 <llesmana@ucsd.edu>
2014-09-04 01:00:26 -0700

    bugs bugs and bugs

--- index.php
+++ index.php
@@ -73,12 +73,7 @@
 
 <div class="container">
 
-    <div class="hero-unit span6">
-        <h1>ErrorMaster the Error Collector</h1>
-        <p> Collect Client side errors, using this simple and easy tool</p>
-    </div>
-
-    <div class="sign-up span3">
+    <div class="sign-up">
         <form class="form-signin" action="./php/signup.php" role="form" method="POST">
             <h2 class="form-signin-heading">Sign Up</h2>
             <input type="text" class="form-control" placeholder="First Name"  name='first' required autofocus>
