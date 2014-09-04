9ae68344e50205b2a06591cb43e29f1b94d8b82c
xxvii27 <llesmana@ucsd.edu>
2014-09-03 21:42:55 -0700

    get types and total

--- php/sort.php
+++ php/sort.php
@@ -166,6 +166,72 @@ function reloadErrorsByOption($master, $option){
     }
 }
 
+function printUserAdmin($username, $status, $num_of_errors, $total_errors){
+
+    echo "<tr>";
+    print "<td> $username </td>";
+    echo  "<td>$num_of_errors</td>";
+    echo "<td>$total_errors</td>";
+    echo "<td>";
+
+    if($status)
+        echo "<span class='staton'>Online";
+    else
+        echo "<span class='statoff'>Offline";
+
+    echo"</span>" ;
+    echo "<button type='button' class='btn btn-default pull-right delete'><span class='glyphicon glyphicon-minus'></span></button>";
+    echo "<button type='button' class='btn btn-default pull-right edit'><span class='glyphicon glyphicon-cog'></span></button></td>";
+    echo"</tr>";
+}
+
+
+function printOwnerAdmin($username, $status){
+    echo "<tr>";
+    print "<td> $username </td>";
+    echo  "<td></td>";
+    echo "<td></td>";
+    echo "<td>";
+
+    if($status)
+        echo "<span class='staton'>Online";
+    else
+        echo "<span class='statoff'>Offline";
+
+    echo"</td>";
+    echo"</tr>";
+}
+
+function reloadUsersAdminByOption($username, $option)
+{
+
+
+    $result = mysql_query("SELECT * FROM user WHERE email = '$username'");
+
+    $row = mysql_fetch_array($result);
+
+    printOwnerAdmin($username, $row['status']);
+
+    //Commence Query
+    if($username === "admin@errormaster.com")
+        $queryUser = "SELECT * FROM user WHERE email <> '$username' ORDER BY $option";
+    else
+        $queryUser = "SELECT * FROM members WHERE master = '$username' ORDER BY $option";
+
+    $result = mysql_query($queryUser);
+
+    while ($row = mysql_fetch_array($result)) {
+        $email = $row['email'];
+        $resource = mysql_query("SELECT COUNT(*) FROM errors WHERE master ='$email' ");
+        $total_errors = mysql_result($resource,0);
+        $resource = mysql_query("SELECT COUNT(DISTINCT name) FROM errors WHERE master ='$email'");
+        $type_of_errors = mysql_result($resource,0);
+        printUserAdmin($row['email'], $row['status'], $type_of_errors, $total_errors);
+    }
+
+}
+
+
 
 connectDB();
 $master = $_SESSION['name'];
@@ -211,7 +277,12 @@ if($type === "logs")
     reloadLogByOption($master, $sortby);
 else if($type === "errors")
     reloadErrorsByOption($master, $sortby);
-else
-    reloadUsersByOption($master, $sortby, $admin);
+else{
+   if($master === "admin@errormaster.com")
+     reloadUsersByOption($master, $sortby, $admin);
+   else
+       reloadUsersAdminByOption($master, $sortby);
+
+}
 
 ?>
