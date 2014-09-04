a0b69743fb83c6b036e14450d210f8f269e557fd
xxvii27 <llesmana@ucsd.edu>
2014-09-03 19:06:10 -0700

    add star ui

--- dash.php
+++ dash.php
@@ -163,16 +163,17 @@ $access = $_SESSION['access'];
                 <div class="input-group">
                     <form>
                         <textarea class="form-control" placeholder="Comments" id='comment' rows="10" cols="100"></textarea>
-                        <div class="rating text-right">
+                        <div class="rating pull-right">
                             <input type="radio" id="star5" name="rating" value="5" /><label for="star5" title="Rocks!">5 stars</label>
                             <input type="radio" id="star4" name="rating" value="4" /><label for="star4" title="Pretty good">4 stars</label>
                             <input type="radio" id="star3" name="rating" value="3" /><label for="star3" title="Meh">3 stars</label>
                             <input type="radio" id="star2" name="rating" value="2" /><label for="star2" title="Kinda bad">2 stars</label>
                             <input type="radio" id="star1" name="rating" value="1" /><label for="star1" title="Sucks big time">1 star</label>
                         </div>
-                        <button type="button" class="btn btn-primary pull-right" id="sendComment">Comment</button>
+                        <button type="button" class="btn btn-primary" id="sendComment">Comment</button>
                     </form>
                 </div>
+                <hr>
                 <div id="comments">
 
                 </div>
