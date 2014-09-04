9d067ccf3346b18b31d8f868c9b25de9dc6edeed
xxvii27 <llesmana@ucsd.edu>
2014-09-04 04:46:05 -0700

    last feature delete comment

--- php/comment.php
+++ php/comment.php
@@ -75,7 +75,7 @@ function reloadComments($err_id){
         }
         echo "</span>";
         echo "<p>".$row['comment']."</p>";
-        echo "<button class='btn btn-xs btn-danger glyphicon glyphicon-trash pull-right rmComment'></button>";
+        echo "<button class='btn btn-xs btn-danger glyphicon glyphicon-trash text-right rmComment'></button>";
         echo "<br/><br/>";
 
 
