f223030c46a5cdc0a321157bb72c0f226990a6b7
xxvii27 <llesmana@ucsd.edu>
2014-09-03 20:36:55 -0700

    get types and total

--- php/users.php
+++ php/users.php
@@ -33,11 +33,11 @@ function printUser($username, $status){
 }
 
 
-function printOwner($username, $status){
+function printOwner($username, $status, $num_of_errors, $total_errors){
 	echo "<tr>";
 	print "<td> $username </td>";
-	echo  "<td>0</td>";
-	echo "<td>0</td>";
+	echo  "<td>$num_of_errors</td>";
+	echo "<td>$total_errors</td>";
 	echo "<td>";
 
 	if($status)
@@ -70,7 +70,12 @@ function reloadUsers($username)
     
     $row = mysql_fetch_array($result);
 
-    printOwner($username, $row['status']);
+    $resource = mysql_query("SELECT COUNT(*) FROM errors WHERE master ='$username' ");
+    $total_errors = mysql_result($resource,0);
+    $resource = mysql_query("SELECT COUNT(DISTINCT name) FROM errors");
+    $type_of_errors = mysql_result($resource,0);
+
+    printOwner($username, $row['status'], $type_of_errors, $total_errors);
 
     //Commence Query
     $queryUser = "SELECT * FROM members WHERE master = '$username'";
